5b005f6a6822e6455a10eba765cfde75558b4564
Miguel Angel Guerra Rangel <maiky_260101@hotmail.com>
2020-12-30 13:51:24 -0600

    Practica 10

---
+++ Practica 10/PowerShellDns.ps1
@@ -0,0 +1,16 @@
+﻿<#
+No es necesario crear el codigo, porque al final son comandos muy sencillos,
+en pocas palabras, lo hice por gusto solamente.
+#>
+
+Write-Host "Obteniendo el DNS del cache (comando: 'Get-DnsClientCache')..."
+$dns1 = Get-DnsClientCache
+
+Write-Host "Obteniendo el DNS del cache (comando: 'ipconfig /displaydns')..."
+$dns2 = ipconfig /displaydns
+
+Write-Host "Creando primer archivo txt..."
+"COMMAND: Get-DnsClientCche `r`n -------------------------------------------------------------------------------------- `r`n", $dns1 | Out-File -FilePath "DNS1.txt"
+
+Write-Host "Creando segundo archivo txt..."
+"COMMAND: ifconfig /displaydns `r`n -------------------------------------------------------------------------------------- `r`n", $dns2 | Out-File -FilePath "DNS2.txt"
